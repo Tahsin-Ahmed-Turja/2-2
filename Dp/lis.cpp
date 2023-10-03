68c068a6ab58842e3fceba828f98056c0577ce06
tahsin030602 <109985922+Tahsin-Ahmed-Turja@users.noreply.github.com>
2023-10-04 03:14:23 +0600

    Update lis.cpp

--- Dp/lis.cpp
+++ Dp/lis.cpp
@@ -31,6 +31,26 @@ vector<int> lis(vector<int> const& a) {
     return subseq;
 }
 
+int lisb(vector<int> const& a) {
+    int n = a.size();
+    const int INF = 1e9;
+    vector<int> d(n+1, INF);
+    d[0] = -INF;
+
+    for (int i = 0; i < n; i++) {
+        int l = upper_bound(d.begin(), d.end(), a[i]) - d.begin();
+        if (d[l-1] < a[i] && a[i] < d[l])
+            d[l] = a[i];
+    }
+
+    int ans = 0;
+    for (int l = 0; l <= n; l++) {
+        if (d[l] < INF)
+            ans = l;
+    }
+    return ans;
+}
+
 int main(){
   ios::sync_with_stdio(false);
   cin.tie(0);
