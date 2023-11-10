9213d2fe6bd4cba8db7eb568202b8b8b08db3e88
tahsin030602 <109985922+Tahsin-Ahmed-Turja@users.noreply.github.com>
2023-11-10 23:24:23 +0600

    Update Edmonds_karp.cpp

--- Graph/Edmonds_karp.cpp
+++ Graph/Edmonds_karp.cpp
@@ -1,73 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int n,m;
-vector<vector<int>> adj;
-vector<vector<int>> capacity;
-
-int bfs(int s, int t, vector<int>& parent) {
+ 
+long long n,m;
+vector<vector<long long>> adj;
+vector<vector<long long>> capacity;
+ 
+ 
+long long bfs(long long s, long long t, vector<long long>& parent) {
     fill(parent.begin(), parent.end(), -1);
     parent[s] = -2;
-    queue<pair<int, int>> q;
-    q.push({s, INT_MAX});
-
+    queue<pair<long long, long long>> q;
+    q.push({s, LLONG_MAX});
+ 
     while (!q.empty()) {
-        int cur = q.front().first;
-        int flow = q.front().second;
+        long long cur = q.front().first;
+        long long flow = q.front().second;
         q.pop();
-
-        for (int next : adj[cur]) {
+ 
+        for (long long next : adj[cur]) {
             if (parent[next] == -1 && capacity[cur][next]) {
                 parent[next] = cur;
-                int new_flow = min(flow, capacity[cur][next]);
+                long long new_flow = min(flow, capacity[cur][next]);
                 if (next == t)
                     return new_flow;
                 q.push({next, new_flow});
             }
         }
     }
-
+ 
     return 0;
 }
-
-int maxflow(int s, int t) {
-    int flow = 0;
-    vector<int> parent(n);
-    int new_flow;
-
+ 
+long long maxflow(long long s, long long t) {
+    long long flow = 0;
+    vector<long long> parent(n+1);
+    long long new_flow;
+ 
     while (new_flow = bfs(s, t, parent)) {
         flow += new_flow;
-        int cur = t;
+        long long cur = t;
         while (cur != s) {
-            int prev = parent[cur];
+            long long prev = parent[cur];
             capacity[prev][cur] -= new_flow;
             capacity[cur][prev] += new_flow;
             cur = prev;
         }
     }
-
+ 
     return flow;
 }
-
-int main() {
+ 
+signed main() {
     cin >> n >> m;
-    adj.resize(n);
-    capacity.resize(n, vector<int>(n));
-
-    int x, y, wt;
+    adj.resize(n+1);
+    capacity.resize(n+1, vector<long long>(n+1));
+ 
+    long long x, y, wt;
     while (m--) {
         cin >> x >> y >> wt;
-        adj[--x].push_back(--y);
-        capacity[x][y] = wt;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+        capacity[x][y]+= wt;
     }
-
-    int ans = maxflow(0, n-1);
-    for(int i=0;i<n;i++){
-        for(int j : adj[i]){
-            if(capacity[i][j]==0){
-                cout << i+1 <<" "<<j+1 << endl;
-            }
-        }
-    }
+ 
+    long long ans = maxflow(1,n);
+    cout << ans << endl;
     return 0;
 }
